f7f5d41bb47a3a335264692403ac3e5b44db09de
randomlysa <su@randomlysa.com>
2017-08-25 00:37:50 -0500

    use redux-save-state to save to localstorage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import saveState from "redux-save-state/localStorage";
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const key = 'weather';
+const createStoreWithMiddleware = applyMiddleware(
+    ReduxPromise, saveState('appState')
+)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>

c9ad0d71189d3f91b12e2aabaa47ee596f0991ed
randomlysa <su@randomlysa.com>
2017-08-27 16:57:51 -0500

    add UVI_URL, fetch weather by cityId

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,17 +4,23 @@ import { loadState } from '../manageLocalStorage';
 import { codes } from 'iso-country-codes';
 
 const API_KEY = '0ef0dca7d078d40465c8a1d8cfd77296';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`
+const WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=metric`;
+const UVI_URL = `http://api.openweathermap.org/data/2.5/uvi?appid=${API_KEY}`; // &lat={lat}&lon={lon}
 
 export const FETCH_WEATHER_FROM_OPENWEATHER = 'FETCH_WEATHER_FROM_OPENWEATHER';
 export const FETCH_WEATHER_FROM_LOCALSTORAGE = 'FETCH_WEATHER_FROM_LOCALSTORAGE';
 
-export function fetchWeatherFromOpenWeather(location) {
-    const items = location.split(",")
-    const [city, country = "United States of America (the)"] = items;
-    const alpha2code = _.filter(codes, {"name": country.trim() })[0].alpha2;
+export function fetchWeatherFromOpenWeather(location, cityId) {
+    let url;
+    if (cityId) {
+        url = `${WEATHER_URL}&id=${cityId}`;
+    } else {
+        const items = location.split(",")
+        const [city, country = "United States of America (the)"] = items;
+        const alpha2code = _.filter(codes, {"name": country.trim() })[0].alpha2;
 
-    const url = `${ROOT_URL}&q=${city},${alpha2code}`;
+        url = `${WEATHER_URL}&q=${city},${alpha2code}`;
+    }
     const request = axios.get(url);
 
     return {

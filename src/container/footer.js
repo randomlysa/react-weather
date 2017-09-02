7ff6a7cbf7c7f9116dbe390f9c169cb037371df5
randomlysa <su@randomlysa.com>
2017-09-02 01:13:19 -0500

    minor ui changes

--- src/container/footer.js
+++ src/container/footer.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
             <div className="row">
               <div className="col">
                   <button onClick={clearStorage} className="btn btn-danger btn-sm">Delete All Cities / Clear localstorage)</button>
-                  &nbsp; (This may be necessary if you are using an older version of this app.)
+                  (This may be necessary if you are using an older version of this app.)
               </div>
             </div>
         );

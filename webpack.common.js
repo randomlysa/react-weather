31980c54b1ce6b79c5ac666c768af3eccb09fa93
randomlysa <su@randomlysa.com>
2017-10-06 21:15:30 -0500

    update webpack/package cfg, install dev packages

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: [
+      './src/index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      loaders: [{
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx']
+    }
+  };

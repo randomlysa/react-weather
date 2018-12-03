3478fa719a7bcefe6eab9e9e631a099485f71f44
randomlysa <su@randomlysa.com>
2018-12-03 14:58:37 -0600

    add material-icons, add settings icon

--- src/container/footer.js
+++ src/container/footer.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import { clearStorage } from '../manageLocalStorage';
 
+import MaterialIcon, { colorPalette } from 'material-icons-react';
+
 export default class Footer extends Component {
   render() {
     return (
       <div className="row row--footer">
         <div className="col">
-          <button onClick={clearStorage} className="btn btn-danger btn-sm">
-            Delete All Cities
-          </button>
+          <span className="icon-settings">
+            <MaterialIcon icon="settings" size="medium" />
+          </span>
         </div>
       </div>
     );
   }
 }
+
+// <button onClick={clearStorage} className="btn btn-danger btn-sm">
+// Delete All Cities
+// </button>

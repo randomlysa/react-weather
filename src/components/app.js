6e0d997be579b92a02a2389a410ef9363573ce71
randomlysa <su@randomlysa.com>
2018-12-04 13:12:59 -0600

    close dropup menu when clicking on body but not when clicking on menu

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,22 @@ import Footer from '../container/footer';
 import CityList from '../container/city-list';
 
 export default class App extends Component {
+  componentDidMount() {
+    const menu = document.getElementsByClassName('settings-menu__content')[0];
+    const body = document.getElementById('body');
+
+    body.addEventListener('click', e => {
+      const obj = e.target.classList;
+      // Convert classList into an array.
+      const classes = Object.keys(obj).map(key => obj[key]);
+
+      // If the area clicked was not 'settings-menu__content', close
+      // the dropup menu.
+      if (!classes.includes('settings-menu__content')) {
+        menu.classList.remove('settings-menu__content--display');
+      }
+    });
+  }
   render() {
     return (
       <div id="app">

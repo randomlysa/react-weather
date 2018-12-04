1764300a2af217421e4110509c3dfe8b533a0f9b
randomlysa <su@randomlysa.com>
2018-12-04 12:13:37 -0600

    create basic dropup menu

--- src/container/footer.js
+++ src/container/footer.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-import { clearStorage } from '../manageLocalStorage';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { deleteCity } from '../actions/index';
 
 import MaterialIcon, { colorPalette } from 'material-icons-react';
 
-export default class Footer extends Component {
+class Footer extends Component {
+  toggleMenu() {
+    const menu = document.getElementsByClassName('settings-menu__content')[0];
+    menu.classList.toggle('settings-menu__content--display');
+  }
+  deleteAllCities() {
+    this.props.cities.map(city => {
+      // this.props.deleteCity(city.id)
+    });
+  }
+
   render() {
     return (
       <div className="row row--footer">
         <div className="col">
-          <span className="icon-settings">
-            <MaterialIcon icon="settings" size="medium" />
-          </span>
+          <div className="settings-menu">
+            <button className="icon-settings" onClick={this.toggleMenu}>
+              <MaterialIcon icon="settings" size="medium" />
+            </button>
+            <div className="settings-menu__content">
+              <p onClick={this.deleteAllCities.bind(this)}>Delete All Cities</p>
+              <p>Disable Swipe to Delete</p>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
 
-// <button onClick={clearStorage} className="btn btn-danger btn-sm">
-// Delete All Cities
-// </button>
+function mapStateToProps(state) {
+  return { cities: state.weather };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ deleteCity }, dispatch);
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Footer);

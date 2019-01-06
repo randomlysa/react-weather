96dcd09819417a4066a0d1ceac24b1c4014f29fb
randomlysa <su@randomlysa.com>
2019-01-05 19:40:20 -0600

    hide checkbox and use line-through instead

--- src/components/option.js
+++ src/components/option.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 export default props => {
   return (
-    <div>
-      <label className="btn btn-link">
-        <input
-          type="checkbox"
-          onChange={props.updateCheckbox}
-          checked={props.isChecked || false}
-        />
-        {props.text}
-      </label>
-    </div>
+    <label className="btn btn-link">
+      <input
+        type="checkbox"
+        onChange={props.updateCheckbox}
+        checked={props.isChecked || false}
+      />
+      <div>{props.text}</div>
+    </label>
   );
 };

be54760fee8cafd55862d8dcc718ac89386b8bb3
randomlysa <su@randomlysa.com>
2019-01-08 09:28:25 -0600

    change weather font size when > 768px
    previously, there was a lot of whitespace and the page looked very blank with just the default options (show C / F)

--- src/components/weather-data.js
+++ src/components/weather-data.js
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 
 const WeatherData = styled.span`
   padding-left: 20px;
+
+  font-size: 2rem;
+
+  @media (min-width: 768px) {
+    font-size: 5rem;
+  }
 `;
 
 // Used to display C / F / Humidity

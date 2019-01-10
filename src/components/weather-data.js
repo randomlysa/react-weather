ab1967a25b5c271dbbfa6d4e5bb17b0e677359a9
randomlysa <su@randomlysa.com>
2019-01-09 23:42:22 -0600

    conditional styling - keep humidity same size, change temp size
    always keep humidity at 1.3rem but change the size of the temp based on the screen size

--- src/components/weather-data.js
+++ src/components/weather-data.js
@@ -3,19 +3,24 @@ import styled from 'styled-components';
 
 const WeatherData = styled.span`
   padding-left: 20px;
-
-  font-size: 2rem;
+  font-size: ${props => (props.label ? '1.3rem' : '2rem')};
 
   @media (min-width: 768px) {
-    font-size: 5rem;
+    font-size: ${props => (props.label ? '1.3rem' : '5rem')};
   }
 `;
 
 // Used to display C / F / Humidity
 export default props => {
+  // Only humidity has a label. Keep font size of 1.3 for humidity
+  // but change font-size of temp based on screen size.
+  // Also put a space between number and C/F but not between number
+  // and % humidity. (1 C  34F  35% humidity)
   return (
-    <WeatherData>
-      {props.data} {props.units} {props.label}
+    <WeatherData label={props.label}>
+      {props.data}
+      {!props.label && ' '}
+      {props.units} {props.label}
     </WeatherData>
   );
 };

583f2fef14eef26ee1ddfdec47e1991db653e373
randomlysa <su@randomlysa.com>
2019-01-11 23:28:20 -0600

    add margin to bottom of page

--- src/container/Cities/weather-list.js
+++ src/container/Cities/weather-list.js
@@ -17,6 +17,7 @@ import GoogleMap from '../../components/google-map';
 const StyledWeatherList = styled.div`
   display: grid;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  margin-bottom: 4rem;
 `;
 
 const WeatherText = styled.div`
